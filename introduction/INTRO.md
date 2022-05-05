92a9643837122c4c435d819df91744ee995c4169
Ronald Gonzales <ronagonzal@gmail.com>
2022-05-05 05:33:01 -0500

    Exercise of Static Initialization Blocks

---
+++ introduction/INTRO.md
@@ -0,0 +1,6 @@
+# Introduction
+
+**StaticInitializer** : To test knowledge of Static initialization blocks.
+
+https://www.freecodecamp.org/news/java-static-keyword-explained
+https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
---
+++ introduction/StaticInitializer.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+/**
+ * Java Static Initializer Block
+ * These blocks are executed when the class is loaded, and here can initialize static variables
+ */
+public class StaticInitializer {
+    public static int B;
+    public static int H;
+    public static boolean flag;
+    
+    static {
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("Enter the BREADTH of the parallelogram:");
+        B = Integer.parseInt(scanner.nextLine());
+        //System.out.println("Enter the HEIGHT of the parallelogram:");
+        H = Integer.parseInt(scanner.nextLine());
+        if(B > 0 && H > 0) {
+            flag = true;
+        }else {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        scanner.close();
+    }
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}
+
+}
+
